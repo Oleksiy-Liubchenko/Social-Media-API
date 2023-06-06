ee4a0501a87d3b32b09eb50072967778441c0473
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-06-06 19:49:53 +0300

    Add str methods to post model & register user admin

--- post/models.py
+++ post/models.py
@@ -6,9 +6,15 @@ from Social_Media_API import settings
 class Hashtag(models.Model):
     name = models.CharField(max_length=120)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     content = models.TextField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     hashtag = models.ManyToManyField(Hashtag)
+
+    def __str__(self):
+        return self.content
--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from post.models import Hashtag, Post
+from user.models import User
 
 
-# admin.site.register(Hashtag)
-# admin.site.register(Post)
+admin.site.register(User)
