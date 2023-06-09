c99d5abe6d24a6c2d1bd1ab1b7383372efc4c171
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-06-09 17:21:36 +0300

    Add media setting for images

--- Social_Media_API/urls.py
+++ Social_Media_API/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
@@ -23,9 +24,12 @@ from drf_spectacular.views import (
 )
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from Social_Media_API import settings
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/social_media/", include("post.urls")),
+    path("api/user/", include("user.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/schema/swagger-ui/",
@@ -37,6 +41,4 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
