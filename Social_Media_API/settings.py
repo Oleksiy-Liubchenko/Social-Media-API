7c2ec79a2435d7acebbd134f4bf031c1969244b1
Oleksiy Liubchenko <alexeylubchenko75@gmail.com>
2023-06-13 17:39:56 +0300

    Upate lifetime access token to 90min

--- Social_Media_API/settings.py
+++ Social_Media_API/settings.py
@@ -151,7 +151,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=90),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
 }
